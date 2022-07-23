2288ecb8ec36b25de0d5b656b228941bd9584e45
HeannaReis <67883256+HeannaReis@users.noreply.github.com>
2022-07-23 16:59:25 -0300

    add comparaNumeros

---
+++ sintaxe e operadores/comparaNumeros.js
@@ -0,0 +1,44 @@
+function comparaNumeros(num1, num2) {
+	const primeiraFrase = criaPrimeiraFrase(num1, num2);
+	const segundaFrase = criaSegundaFrase(num1, num2);
+
+	return `${primeiraFrase} ${segundaFrase}`;
+}
+
+function criaPrimeiraFrase(num1, num2) {
+	let primeiraFrase = `Os números ${num1} e ${num2}`;
+	let simNao = 'não';
+
+	if (num1 === num2) {
+		simNao = '';
+	}
+
+	return `${primeiraFrase} ${simNao} são iguais.`;
+}
+
+function criaSegundaFrase(num1, num2) {
+	const soma = num1 + num2;
+	let comparaDez = 'menor';
+	let comparaVinte = 'menor que';
+
+	if (soma === 10) {
+		comparaDez = 'igual a';
+	}
+
+    if (soma > 10) {
+		comparaDez = 'maior do que';
+	}
+
+	if (soma === 20) {
+		comparaVinte = 'igual a';
+	}
+
+    if (soma > 20) {
+		comparaVinte = 'maior do que';
+	}
+
+
+	return `Sua soma é ${soma}, que é ${comparaDez} 10 e ${comparaVinte} 20.`;
+}
+
+console.log(comparaNumeros(5, 5));
