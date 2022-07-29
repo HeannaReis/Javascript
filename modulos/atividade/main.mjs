edad8e1da94ddc77a4656eac8ed58a9cfd151304
HeannaReis <67883256+HeannaReis@users.noreply.github.com>
2022-07-28 21:50:08 -0300

    update metodos

---
+++ modulos/atividade/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Joel', 36));
+console.log(mostraCidade('Joel', 'São Paulo'));
+console.log(mostraHobby('Gabriel', 'Jogar vídeo game'));
